62c1d3ea0be2894ffb1954a2f56fc61528bd1bfd
Liam Bailey <ljbailey17@gmail.com>
2023-09-05 11:39:59 +0100

    server.js and mongo comms

---
+++ server/db/seeds.js
@@ -0,0 +1,19 @@
+use podcast_db;
+db.dropDatabase();
+
+db.list.insertMany([
+  {
+    "uuid": "f9ef95c3-061e-4162-b122-7077bf8985b0",
+        "name": "Men Who Talk",
+        "description": "A Man's Perspective. Join us as we share and discuss some of our biggest challenges but also our biggest WINS! Men don't talk enough so this is our opportunity. ",
+        "genres": [
+          "Self Improvement",
+          "Education"
+        ],
+        "itunesInfo": {
+          "uuid": "f9ef95c3-061e-4162-b122-7077bf8985b0",
+          "baseArtworkUrlOf": "https://is1-ssl.mzstatic.com/image/thumb/Podcasts125/v4/e4/36/81/e4368170-5e77-b289-9b6f-9a436e301e40/mza_8886635448311323595.jpg/640x640bb.png"
+        }
+      }
+  
+]);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors');
+app.use(cors())
 app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true})
+  .then((client) => {
+    const db = client.db('podcast_db');
+    const listCollection = db.collection('list');
+    const individualCollection = db.collection('individual');
+    const listRouter = createRouter(listCollection);
+    app.use('api/list', listRouter);
+    const individualRouter = createRouter(individualCollection);
+    app.use('api/individual', individualRouter);
+  })
+    .catch(console.err);
+
+
 
 app.listen(9000, function () {
 	console.log(`Listening on port ${this.address().port}`);
